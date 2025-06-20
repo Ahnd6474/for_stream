7dc160ee945cc1044fb50a7a2bc1a9824de13239
Danny <ahnd6474@gmail.com>
2025-06-20 15:30:34 +0900

    Update app_eda.py

--- app_eda.py
+++ app_eda.py
@@ -333,7 +333,7 @@ class EDA:
         st.pyplot(fig)
 
 # 실제 사용 예
-home = Home(Page_Login, Page_Register, Page_FindPW)
+home = Home(Login, Register, FindPassword)
 if 'data_loaded' not in st.session_state:
     st.session_state['data_loaded'] = False
 
