876d825e96984f5ac5324f030c728064528354a5
Danny <ahnd6474@gmail.com>
2025-06-20 15:39:22 +0900

    Update app_eda.py

--- app_eda.py
+++ app_eda.py
@@ -345,15 +345,15 @@ Page_EDA      = st.Page(EDA,      title="EDA",     icon="📊", url_path="eda")
 # 네비게이션 실행
 # ---------------------
 # 실제 사용 예
-'''
-home = Home(Login, Register, FindPassword)
+
+#home = Home(Login, Register, FindPassword)
 if 'data_loaded' not in st.session_state:
     st.session_state['data_loaded'] = False
     
 if st.session_state['data_loaded'] or st.button("Start EDA"):
     st.session_state['data_loaded'] = True
     eda = EDA()   
-'''
+
 if st.session_state.logged_in:
     pages = [Page_Home, Page_User, Page_Logout, Page_EDA]
 else:
